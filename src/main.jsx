9aca3332ab23067f6d159e20323961b59188f193
miz-app-builder <barilaptop1@gmail.com>
2026-09-17 22:18:22 +0600

    feat: use app shell with bazar integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import AppShell from './AppShell.jsx';
 import './styles.css';
 import './meal.css';
 import './bazar.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppShell />
   </React.StrictMode>
 );
